87324fe980eba5f883ce70ea1f1e0f75c656d0e4
Tony sh <32256254+NorthForge@users.noreply.github.com>
2021-01-27 11:59:29 +0500

    test

--- contact.php
+++ contact.php
@@ -1,3 +1,30 @@
+<?php
+// Free html5 templates : https://www.zerotheme.com
+
+$text = "<span style='color:red; font-size: 35px; line-height: 40px; magin: 10px;'>Error! Please try again.</span>";
+
+if(isset($_POST['submitcontact']))
+{
+	$name=$_POST['name'];
+	$email=$_POST['email'];
+	$message=$_POST['message'];
+	$subject=$_POST['subject'];
+
+	$to = "youremail@gmail.com";
+	$subject = "Zerotheme - Testing Contact Form";
+	$message = " Name: " . $name ."\r\n Email: " . $email . "\r\n Message:\r\n" . $message;
+	 
+	$from = "Zerotheme dot com";
+	$headers = "From:" . $from . "\r\n";
+	$headers .= "Content-type: text/plain; charset=UTF-8" . "\r\n"; 
+	 
+	if(@mail($to,$subject,$message,$headers))
+	{
+	  $text = "<span style='color:blue; font-size: 35px; line-height: 40px; margin: 10px;'>Your Message was sent successfully !</span>";
+	}
+}
+?>
+
 <!DOCTYPE html>
 <!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
 <!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
@@ -86,7 +113,7 @@
 					<!-- Menu-main -->
 					<div id='cssmenu' class="align-center">
 						<ul>
-						   <li class="active"><a href='index.html'><span>Home</span></a></li>
+						   <li><a href='index.html'><span>Home</span></a></li>
 						   <li class=' has-sub'><a href='#'><span>Categories</span></a>
 							  <ul >
 								 <li class='has-sub'><a href='#'><span>Item 1</span></a>
@@ -105,7 +132,7 @@
 						   </li>
 						   <li><a href='single.html'><span>About</span></a></li>
 						   <li><a href='archive.html'><span>Blog</span></a></li>
-						   <li class='last'><a href='contact.html'><span>Contacts</span></a></li>
+						   <li class='last active'><a href='contact.html'><span>Contacts</span></a></li>
 						</ul>
 					</div>
 				</div>
@@ -115,62 +142,51 @@
 		<!--////////////////////////////////////Container-->
 		<section id="container">
 			<div class="wrap-container">
-				<div id="main-content">
-					<div class="wrap-content">
-						<div class="zerogrid">
-							<div class="row">
-								<div class="col-1-5">
-									<div class="wrap-col">
-										<a class="portfolio-box zoom-effect" href="single.html">
-											<img src="images/2й вариант.png" class="img-responsive" alt=""> <!-- Картинка 1 -->
-											<div class="portfolio-box-caption">
-												<div class="portfolio-box-caption-content">
-													<div class="project-name">
-														<!--<span>#ANIMAL</span>-->
-													</div>
-												</div>
+				<!-----------------Content-Box-------------------->
+				<section class="content-box zerogrid">
+					<div class="row wrap-box"><!--Start Box-->
+					
+						<!--Start Map-->
+						<div id="map" style="height: 350px;"></div>
+						<!--End Map-->
+						
+						<div class="contact-form">
+							<h3 class="t-center">Contact Form</h3>
+							<!--Warning-->
+							<center><?php echo $text;?></center>
+							<!---->
+							<div id="contact_form">
+								<form name="form1" id="ff" method="post" action="contact.php">
+									<label class="row">
+										<div class="col-1-3">
+											<div class="wrap-col">
+												<input type="text" name="name" id="name" placeholder="Enter name" required="required" />
 											</div>
-										</a>
-									</div>
-								</div>
-								<div class="col-1-5">
-									<div class="wrap-col">
-										<a class="portfolio-box zoom-effect" href="single.html">
-											<img src="images/Avatar02.png" class="img-responsive" alt=""><!-- Картинка 2 -->
-											<div class="portfolio-box-caption">
-												<div class="portfolio-box-caption-content">
-													<div class="project-name">
-														<!--<span>#ANIMAL</span>-->
-													</div>
-												</div>
+										</div>
+										<div class="col-1-3">
+											<div class="wrap-col">
+												<input type="email" name="email" id="email" placeholder="Enter email" required="required" />
 											</div>
-										</a>
-										<a class="portfolio-box zoom-effect" href="single.html">
-											<img src="images/Knight.gif" class="img-responsive" alt=""><!-- Картинка 3 -->
-											<div class="portfolio-box-caption">
-												<div class="portfolio-box-caption-content">
-													<div class="project-name">
-														<!--<span>#ANIMAL</span>-->
-													</div>
-												</div>
+										</div>
+										<div class="col-1-3">
+											<div class="wrap-col">
+												<input type="text" name="subject" id="subject" placeholder="Subject" required="required" />
 											</div>
-										</a>
-										<a class="portfolio-box zoom-effect" href="single.html">
-											<img src="images/MLevel2Color.png" class="img-responsive" alt=""><!-- Картинка 4 -->
-											<div class="portfolio-box-caption">
-												<div class="portfolio-box-caption-content">
-													<div class="project-name">
-														<!--<span>#ANIMAL</span>-->
-													</div>
-												</div>
-											</div>
-										</a>
-									</div>
-								<!--</div>-->
+										</div>
+									</label>
+									<label class="row">
+										<div class="wrap-col">
+											<textarea name="message" id="message" class="form-control" rows="4" cols="25" required="required"
+											placeholder="Message"></textarea>
+										</div>
+									</label>
+									<center><input class="sendButton" type="submit" name="submitcontact" value="Submit"></center>
+								</form>
 							</div>
 						</div>
 					</div>
-				</div>
+				</section>
+				
 			</div>
 		</section>
 		<!--////////////////////////////////////Footer-->
@@ -189,27 +205,74 @@
 					</div>
 				</div>
 				<div class="copyright">
-					Copyright @ - Designed by <a href="https://www.zerotheme.com">ZEROTHEME</a>
+					Copyright @ - Designed by <a href="https://www.zerotheme.com" title="free website templates">ZEROTHEME</a>
 				</div>
 			</div>
 		</footer>
-		<!-- carousel -->
-		<script src="owl-carousel/owl.carousel.js"></script>
-		<script>
-		$(document).ready(function() {
-		  $("#owl-slide").owlCarousel({
-			autoPlay: 3000,
-			items : 1,
-			itemsDesktop : [1199,1],
-			itemsDesktopSmall : [979,1],
-			itemsTablet : [768, 1],
-			itemsMobile : [479, 1],
-			navigation: true,
-			navigationText: ['<i class="fa fa-chevron-left fa-5x"></i>', '<i class="fa fa-chevron-right fa-5x"></i>'],
-			pagination: false
-		  });
-		});
-		</script>
+		
+		<!-- Google Map -->
+	<script>
+	  var marker;
+	  var image = 'images/map-marker.png';
+      function initMap() {
+        var myLatLng = {lat: 39.79, lng: -86.14};
+
+		// Specify features and elements to define styles.
+        var styleArray = [
+          {
+            featureType: "all",
+            stylers: [
+             { saturation: -80 }
+            ]
+          },{
+            featureType: "road.arterial",
+            elementType: "geometry",
+            stylers: [
+              { hue: "#000000" },
+              { saturation: 50 }
+            ]
+          },{
+            featureType: "poi.business",
+            elementType: "labels",
+            stylers: [
+              { visibility: "off" }
+            ]
+          }
+        ];
+		
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: myLatLng,
+          scrollwheel: false,
+		   // Apply the map style array to the map.
+          styles: styleArray,
+          zoom: 7
+        });
+
+        var directionsDisplay = new google.maps.DirectionsRenderer({
+          map: map
+        });
+
+		// Create a marker and set its position.
+        marker = new google.maps.Marker({
+          map: map,
+		  icon: image,
+		  draggable: true,
+          animation: google.maps.Animation.DROP,
+          position: myLatLng
+        });
+		marker.addListener('click', toggleBounce);
+      }
+	  
+	  function toggleBounce() {
+        if (marker.getAnimation() !== null) {
+          marker.setAnimation(null);
+        } else {
+          marker.setAnimation(google.maps.Animation.BOUNCE);
+        }
+      }
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB7V-mAjEzzmP6PCQda8To0ZW_o3UOCVCE&callback=initMap" async defer></script>
+	
 	</div>
 </body>
 </html>
